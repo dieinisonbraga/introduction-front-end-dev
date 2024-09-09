4e73067ee322436c5b87e01ed754abdf333c3946
Dieinison J F Braga <dieinisonbraga@gmail.com>
2024-09-09 14:15:24 -0300

    Mais algumas alterações de funcionamento e estilo

--- hello-angular/src/app/product/product.component.ts
+++ hello-angular/src/app/product/product.component.ts
@@ -12,4 +12,28 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class ProductComponent {
   @Input() product!: Product;
+  quantity: number = 1;
+  statusButton: boolean = true;
+
+  // Função para subtrair o valor da quantidade de um produto
+  minus(): void {
+    (this.quantity - 1) === 1 ? this.statusButton=true : this.statusButton=false;
+    if (this.quantity !== 1) {
+      this.quantity-=1;
+    }
+}
+
+  // Função para somar valor da quantidade de um produto
+  plus(): void {
+    this.statusButton=false;
+    this.quantity++;
+  }
+
+  // Função para comprar um produto informando o preço final
+  buyProduct():  void {
+      if (window.confirm(`Preço final: ${Math.round((this.product.price * this.quantity) * 100) / 100}`)) {
+          alert('Produto comprado com sucesso!')
+          this.quantity = 1;
+      }
+  }
 }
